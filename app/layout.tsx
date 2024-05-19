2eca5de1fc4c2e58789955b5fe9d528dbda0e300
gaurangkhera5188 <khera.gaurang@gmail.com>
2024-05-19 12:07:39 +0530

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,11 @@ import { BackgroundBeams } from "@/components/background-beams";
 import ConvexClientProvider from "@/components/convex-client-provider";
 
 export const metadata: Metadata = {
-  title: "hi, I'm Gaurang.",
-  description: "I'm a 15-year-old student and software engineer based in India. I enjoy playing basketball, math and coding. I'm currently working on a few projects, including this website. I'm currently studying in Grade 10 at Sanskriti School, New Delhi. I'm a hard-working student and have a keen interest in mathematics, physics and computer science. I've been securing top grades in my class since I was 10, and I'm always eager to learn new things."
+  title: "Gaurang | 15-Year-Old Student & Software Engineer",
+  description: "Meet Gaurang, a 15-year-old student and software engineer from India. Passionate about basketball, math, and coding, he's currently working on various projects, including this website. Gaurang excels academically, maintaining top grades in Grade 10 at Sanskriti School, New Delhi. With a keen interest in mathematics, physics, and computer science, he's always eager to learn and grow.",
+  keywords: "blockchain physics math gaurang khera student software sanskriti school basketball lebron james steph curry nike adidas india new delhi government of india startups narendra modi coding programming web development software engineering machine learning artificial intelligence ai deep learning neural networks tensorflow pytorch react nextjs javascript typescript python c++ c# unity game development web3 blockchain ethereum convex finance convex finance",
 }
+
 
 export default function RootLayout({
   children,
@@ -21,7 +23,7 @@ export default function RootLayout({
       <ConvexClientProvider>
         <body className={cn(GeistSans.className, "transform-gpu h-screen")}>
           <Navbar />{" "}
-          <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>{" "}
+          <div className="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>{" "}
           {children} <BackgroundBeams />
         </body>
       </ConvexClientProvider>
