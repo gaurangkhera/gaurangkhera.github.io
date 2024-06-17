b23bcbf9f91411627e9fd99c0831256bfca437b5
Gaurang Khera <khera.gaurang@gmail.com>
2024-06-17 13:40:26 +0530

    seow

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/sidebar";
-import { GeistSans } from "geist/font/sans";
+import { Manrope } from 'next/font/google'
+import { GeistSans } from 'geist/font/sans'
 import { cn } from "@/lib/utils";
 import { BackgroundBeams } from "@/components/background-beams";
 import ConvexClientProvider from "@/components/convex-client-provider";
 
 export const metadata: Metadata = {
-  title: "Gaurang | 15-Year-Old Student & Software Engineer",
-  description: "Meet Gaurang, a 15-year-old student and software engineer from India. Passionate about basketball, math, and coding, he's currently working on various projects, including this website. Gaurang excels academically, maintaining top grades in Grade 10 at Sanskriti School, New Delhi. With a keen interest in mathematics, physics, and computer science, he's always eager to learn and grow.",
-  keywords: "blockchain physics math gaurang khera student software sanskriti school basketball lebron james steph curry nike adidas india new delhi government of india startups narendra modi coding programming web development software engineering machine learning artificial intelligence ai deep learning neural networks tensorflow pytorch react nextjs javascript typescript python c++ c# unity game development web3 blockchain ethereum convex finance convex finance",
+  title: "hey there.",
+  description: "hi, i'm Gaurang. i'm 15 and enjoy programming, basketball and running. i've worked as a SWE and DS for the government of India as well as a startup. i'm currently working on a few projects in fintech.",
+  keywords: "Gaurang Khera, Student Software Engineer, Web Development, AI, Machine Learning, Blockchain, Physics, Math, Coding, JavaScript, TypeScript, Python, React, Next.js, TensorFlow, PyTorch",
 }
+
+const font = Manrope({ subsets: ['latin'] })
 
 
 export default function RootLayout({
